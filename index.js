5f278e32c094c08115fc410587adad1bdaf5cef9
edwardz8 <tolklorist@gmail.com>
2021-05-30 13:12:56 -0400

    prospects added. add scheduling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {
     TwitterClient
 } = require('twitter-api-client')
 const axios = require('axios')
+// const cron = require('node-cron')
 
 const twitterClient = new TwitterClient({
     apiKey: process.env.TWITTER_API_KEY,
@@ -11,25 +12,30 @@ const twitterClient = new TwitterClient({
     accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-axios.get('https://statsapi.web.nhl.com/api/v1/draft/prospects')
-    .then(response => {
-        const data = response.data.prospects ? response.data : {}
-        let tweet
-        if (data && data.prospects.length) {
-            //tweet the first event in the array
-            tweet = '🏒  Hockey Prospect Highlight  🏒  ' + data.prospects[0].fullName + ' - ' + data.prospects[0].birthCountry + ' - ' + data.prospects[0].height + ' - ' + data.prospects[0].weight + ' - ' + data.prospects[0].primaryPosition.name + '  🥅  #nhl #hockey #nhlprospects'
-        } else {
-            tweet = 'No prospect today'
-        }
+setInterval(tweetScheduler, 100 * 10)
 
-        // send tweet
-        twitterClient.tweets.statusesUpdate({
-            status: tweet
-        }).then(response => {
-            console.log('Tweeted', response)
+function tweetScheduler() {
+    axios.get('https://statsapi.web.nhl.com/api/v1/draft/prospects')
+        .then(response => {
+            const data = response.data.prospects ? response.data : {}
+            let tweet
+            if (data && data.prospects.length) {
+                // console.log(data, 'data')
+                tweet = '🏒  Hockey Prospect Highlight  🏒  ' + data.prospects[0].fullName + ' - ' + data.prospects[0].birthCountry + ' - ' + data.prospects[0].height + ' - ' + data.prospects[0].weight + ' - ' + data.prospects[0].primaryPosition.name +  '  🥅  #nhl #hockey #nhlprospects'
+                //tweet the first event in the array
+            } else {
+                tweet = 'No prospect today'
+            }
+
+            // send tweet
+            twitterClient.tweets.statusesUpdate({
+                status: tweet
+            }).then(response => {
+                console.log('Tweeted', response)
+            }).catch(err => {
+                console.log(err)
+            })
         }).catch(err => {
-            console.log(err)
+            console.error(err)
         })
-    }).catch(err => {
-        console.error(err)
-    })
+}
