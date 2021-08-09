70c09a1108ce8a3436a6b384aeae7f651a9a6003
edwardz8 <tolklorist@gmail.com>
2021-08-08 22:31:08 -0400

    new timeout logic

--- index.js
+++ index.js
@@ -12,16 +12,33 @@ const twitterClient = new TwitterClient({
     accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-setInterval(tweetScheduler, 100 * 10)
+// setInterval(tweetScheduler, 100000)
+
+setTimeout( function(){ 
+    var hour = new Date().getHours();
+    if (hour === 10 || hour === 11 || hour === 3) {
+        tweetScheduler
+    }
+}, 100000);
 
 function tweetScheduler() {
     axios.get('https://statsapi.web.nhl.com/api/v1/draft/prospects')
         .then(response => {
             const data = response.data.prospects ? response.data : {}
             let tweet
+            let count = 0 
+
+            /* function counter() {
+                return ++count
+            } */
+
+            const random = Math.floor(Math.random() * 100);
+
+            // console.log(random);
+
             if (data && data.prospects.length) {
                 // console.log(data, 'data')
-                tweet = '🏒  Hockey Prospect Highlight  🏒  ' + data.prospects[0].fullName + ' - ' + data.prospects[0].birthCountry + ' - ' + data.prospects[0].height + ' - ' + data.prospects[0].weight + ' - ' + data.prospects[0].primaryPosition.name +  '  🥅  #nhl #hockey #nhlprospects'
+                tweet = '🏒  Hockey Prospect Highlight  🏒  ' + data.prospects[0].fullName + ' - ' + data.prospects[0].birthCountry + ' - ' + data.prospects[0].height + ' - ' + data.prospects[0].weight + ' - ' + data.prospects[0].primaryPosition.name + random +  '  🥅  #nhl #hockey #nhlprospects'
                 //tweet the first event in the array
             } else {
                 tweet = 'No prospect today'
